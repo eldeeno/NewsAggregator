a43c0c4b4e83307c25ddafae5199cbc45bca6be0
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2025-09-29 15:31:17 +0100

    Added API routes for authentication and article management

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\UserPreferenceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::get('/articles/filters', [ArticleController::class, 'filters']);
+    Route::get('/articles/{article}', [ArticleController::class, 'show']);
+
+    Route::get('/preferences', [UserPreferenceController::class, 'show']);
+    Route::put('/preferences', [UserPreferenceController::class, 'update']);
+});
