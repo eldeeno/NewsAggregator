8033c9fbf1055ff7984cfc53eeb897fb977b9da1
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2025-09-30 01:11:08 +0100

    Change news fetch schedule to daily and adjust overlapping settings

--- routes/console.php
+++ routes/console.php
@@ -9,8 +9,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('news:fetch')->everyFiveMinutes()
-    ->withoutOverlapping()
+Schedule::command('news:fetch')->daily()
+    ->withoutOverlapping(30)
     ->appendOutputTo(storage_path('logs/news-aggregation.log'));
 
 Schedule::command('queue:retry all')->everyFifteenMinutes();
